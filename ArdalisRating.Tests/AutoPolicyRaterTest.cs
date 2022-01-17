16ca6b7ed1de3fe9768a571d7a931da7b0434888
Stas Wasserman <swasserman@orderbot.ca>
2022-01-17 12:52:20 -0800

    Refactoring for ISP

---
+++ ArdalisRating.Tests/AutoPolicyRaterTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ArdalisRating.Tests
+{
+
+    public class FakeLogger : ILogger
+    {
+        public List<string> loggedMessages = new List<string>();
+        public void Log(string message)
+        {
+            loggedMessages.Add(message);
+        }
+    }
+
+    public class FakeRatingUpdater : IRatingUpdater
+    {
+        public decimal? NewRating { get; private set; } 
+        
+        public void UpdateRating(decimal rating)
+        {
+            NewRating = rating;
+        }
+    }
+
+    public class AutoPolicyRaterTest
+    {
+        [Fact]
+        public void LogsProperMessageWhenMakeIsMissing()
+        {
+            var logger = new FakeLogger();
+            var policy = new Policy
+            {
+                Type = PolicyType.Auto.ToString(),
+                Model = "300i",
+                Year = 2000,
+                Miles = 1000,
+                Deductible = 100
+            };
+
+            var sut = new AutoPolicyRater(null);
+            sut.Logger = logger;
+
+            sut.Rate(policy);
+
+            Assert.Equal("Auto policy must specify Make", logger.loggedMessages.Last());
+        }
+
+        [Fact]
+        public void RatingIs1000WhenDeductableLessThan500()
+        {
+            var ratingUpdater = new FakeRatingUpdater();
+            var policy = new Policy
+            {
+                Type = PolicyType.Auto.ToString(),
+                Make = "BMW",
+                Model = "300i",
+                Year = 2000,
+                Miles = 1000,
+                Deductible = 100
+            };
+
+            var sut = new AutoPolicyRater(ratingUpdater);
+
+            sut.Rate(policy);
+
+            Assert.Equal(1000m, ratingUpdater.NewRating);
+        }
+    }
+}
---
+++ ArdalisRating/RatingUpdater.cs
@@ -0,0 +1,17 @@
+﻿namespace ArdalisRating
+{
+    public class RatingUpdater : IRatingUpdater
+    {
+        private RatingEngine _engine;
+
+        public RatingUpdater(RatingEngine engine)
+        {
+            _engine = engine;
+        }
+
+        public void UpdateRating(decimal rating)
+        {
+            _engine.Rating = rating;
+        }
+    }
+}
