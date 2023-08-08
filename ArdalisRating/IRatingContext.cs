f9098596235b50fda9a5ed094166877afdbe8882
Stas Wasserman <swasserman@orderbot.com>
2023-08-08 04:42:20 -0700

    Implemented RatingUpdater; Code tidy up;

--- ArdalisRating/IRatingContext.cs
+++ ArdalisRating/IRatingContext.cs
@@ -1,20 +1,12 @@
 ﻿namespace ArdalisRating
 {
-    public interface IRatingUpdater
+    public interface IRatingContext : ILogger
     {
-        void UpdateRating(decimal rating);
-    }
-    
-    public interface IRatingContext : ILogger, IRatingUpdater
-    {
-        //void Log(string message);
         string LoadPolicyFromFile();
         string LoadPolicyFromURI(string uri);
         Policy GetPolicyFromJsonString(string policyJson);
         Policy GetPolicyFromXmlString(string policyXml);
         Rater CreateRaterForPolicy(Policy policy, IRatingContext context);
-        //void UpdateRating(decimal rating);
         RatingEngine Engine { get; set; }
-        //ConsoleLogger Logger { get; }
     }
 }
--- ArdalisRating/Rater.cs
+++ ArdalisRating/Rater.cs
@@ -2,16 +2,12 @@
 {
     public abstract class Rater
     {
-        //protected readonly IRatingContext _context;
-
         protected readonly IRatingUpdater _ratingUpdater;
-        //protected readonly ConsoleLogger _logger;
         public ILogger Logger { get; set; } = new ConsoleLogger();
 
         public Rater(IRatingUpdater ratingUpdater)
         {
             _ratingUpdater = ratingUpdater;
-            //_logger = _context.Logger;
         }
 
         public abstract void Rate(Policy policy);
